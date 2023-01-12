e457b3cd17fab87fae2ff5ac2603c448c28c4b07
Suren <parsyansuren@gmail.com>
2023-01-13 01:54:07 +0400

    homework-6: fix a failur in mergeArrays

--- src/Homework6/mergeArrays.java
+++ src/Homework6/mergeArrays.java
@@ -5,15 +5,15 @@ import java.util.Arrays;
 public class mergeArrays {
     public static void main(String[] args){
         int[] arr1 = {7, 5, 8, 5, 9, 45, 3};
-        int[] arr2 = {7, 5, 8, 5, 9, 45, 3};
-        int[] array= new int[14];
+        int[] arr2 = {5, 9, 6, 5, 3, 15, 4};
+        int[] array= new int[arr1.length];
 
         for (int i = 0; i < arr1.length; i++) {
             if (arr1[i]%2==0 && arr2[i]%2==0){
-                array[i*2] = arr1[i];
+                array[i] = arr1[i];
             }
-            if (arr2[i]%3==0 && arr1[i]%3==0){
-                array[i*2+1] = arr2[i];
+            if (arr2[i]%2!=0 && arr1[i]%2!=0){
+                array[i] = arr2[i];
             }
         }
 
