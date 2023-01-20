a52d17c0e1ea9c6d53d7b0e37564e9bac322f97d
Suren <parsyansuren@gmail.com>
2023-01-20 04:00:05 +0400

    homework-7: extra homework-2 fix failur

--- src/Homework7/extraHomework_getPrimeNum.java
+++ src/Homework7/extraHomework_getPrimeNum.java
@@ -2,15 +2,17 @@ package Homework7;
 
 public class extraHomework_getPrimeNum {
     public static void main(String[] args) {
-        int[] array = {5,94,45,15,8,9,29,21,9};// 1,2 are out of the rule
+        int[] array = {5,94,45,15,8,11,23,9,29,21,51,9,5,41,43};// 1,2 are out of the rule
         int count = 0;
 
         for (int n: array) {
-            for (int i = 1; i < n/2; i++) {
-                if (n%(i+1) == 0) {
+            for (int i = 2; i <= n/2; i++) {
+                if (n%i == 0) {
+                    break;
+                }
+                else if (i == n/2) {
                     System.out.println(n);
                     count++;
-                    break;
                 }
             }
         }
