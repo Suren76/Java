64cf39e246635f4ea03af5085ff3be3d38c084f2
Suren <parsyansuren@gmail.com>
2023-02-04 18:56:05 +0400

    homework-10: fix failure

--- src/Homework10/GetWordCountInString.java
+++ src/Homework10/GetWordCountInString.java
@@ -1,19 +1,26 @@
 package Homework10;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+
 public class GetWordCountInString {
     public static void main(String[] args){
-        System.out.println(wordCount("gvbdjbn_ad_vhav_ad_fdjn_ad_string","ad"));
+        System.out.println(wordCount("gvbdjbn ad vhav ad fdjn ad string","ad"));
+        System.out.println(stringCount("gvbdjbn ad vhav ad fdjn ad string"));
     }
 
     static int wordCount(String text, String word){
         if (text.length() == 0 || word.length() == 0) return -2;
-        if (!text.contains(word)) return -1;
-        return (text.length() - text.replace(word,"").length())/word.length();
+        int count = 0;
+        for (String wordToCompare:text.split(" ")) if (wordToCompare.equals(word)) count++;
+        return count==0 ? -1: count;
     }
 
     static int stringCount(String text){
         if (text.length() == 0) return -2;
-        if (!text.contains("string")) return -1;
-        return (text.length() - text.replace("string","").length())/"string".length();
+        int count = 0;
+        for (String wordToCompare:text.split(" ")) if (wordToCompare.equals("string")) count++;
+        return count==0 ? -1: count;
     }
 }
