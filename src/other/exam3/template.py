e6338fa2445c96eec60b67c5f1126a129cad8501
Suren <parsyansuren@gmail.com>
2023-03-04 19:01:33 +0400

    exam3

---
+++ src/other/exam3/template.py
@@ -0,0 +1,34 @@
+import os
+
+to_html = ""
+
+for file in [filename for filename in os.listdir() if ".java" in filename]:
+    # print(file)
+    code = open(file).read()
+
+    os.environ["output"] = ""
+    os.system(f"java {file}")
+
+    output = os.environ["output"]
+
+    to_html += f"<output>{str(output)}</output>"
+    print(to_html)
+
+
+open("index.html", "w").write(
+f"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+{to_html}
+</body>
+</html>
+"""
+
+)
+
+
